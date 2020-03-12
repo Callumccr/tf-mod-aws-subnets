35652383b9059ea0278f077a9a820791d9e9cd63
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-12 22:32:43 +0000

    Gitlab README commit

--- label.tf
+++ label.tf
@@ -1,13 +1,12 @@
 module "label" {
   source             = "git::https://github.com/Callumccr/tf-mod-label.git?ref=master"
-  context            = var.context
   namespace          = var.namespace
   environment        = var.environment
   name               = var.name
   attributes         = [""]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
-  label_order        = ["namespace", "environment", "name", "attributes"]
+  label_order        = ["environment", "namespace", "name", "attributes"]
 }
 
 module "private_subnet_label" {
