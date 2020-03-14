f918e4ac642fedb13c305c49e1346526b6cd1840
callumccr@gmail.com <jenkins-x@googlegroups.com>
2020-03-14 20:37:39 +0000

    Gitlab README commit

--- variables.tf
+++ variables.tf
@@ -64,14 +64,9 @@ variable "vpc_id" {
   description = "VPC ID where subnets will be created (e.g. `vpc-aceb2723`)"
 }
 
-variable "igw_id" {
-  type        = string
-  description = "Internet Gateway ID the public route table will point to (e.g. `igw-9c26a123`)"
-}
-
-variable "cidr_block" {
-  type        = string
-  description = "Base CIDR block which will be divided into subnet CIDR blocks (e.g. `10.0.0.0/16`)"
+variable "availability_zones" {
+  description = "(Required) - The AWS avaialbility zones (e.g. ap-southeast-2a/b/c). Autoloaded from region.tfvars."
+  type        = list(string)
 }
 
 variable "vpc_default_route_table_id" {
